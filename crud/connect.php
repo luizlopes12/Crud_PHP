f863009777f7076892cdb4de9ccf59b391b1e2c8
Luiz Lopes <luizlopes12@outlook.com>
2022-01-08 12:59:39 -0300

    Update connect.php

--- crud/connect.php
+++ crud/connect.php
@@ -1,7 +1,7 @@
 <?php
 
-$connect = mysqli_connect("localhost","id17487737_luiz1112","QbjYSLx2ikxpGY#");   //Selecionando o usuario do banco e o ip
-           mysqli_select_db($connect,"id17487737_webdbherois");            //seleionando o nome do banco
+$connect = mysqli_connect("localhost","root","1112");   //Selecionando o usuario do banco e o ip
+           mysqli_select_db($connect,"web");            //seleionando o nome do banco
            mysqli_set_charset($connect,"UTF8");         //Tipos de caracteres
 if(!$connect){
     echo "<script>alert('Banco não conectado')</script>";
